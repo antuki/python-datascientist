4aa36e99f49846481652b084d0c3f0d30fd3dbd2
linogaliana <linogaliana@yahoo.fr>
2021-09-03 15:48:01 +0000

    build setup

---
+++ build/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+REPO_URL=https://github.com/linogaliana/python-datascientist
+
+# Clone the repository in /home/rstudio/tutoriel
+mkdir /home/python-datascientist
+git clone $REPO_URL /home/python-datascientist
