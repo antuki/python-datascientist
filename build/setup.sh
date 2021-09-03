37d6c7ce0ea88b624d91d4b9fd039c7de6693145
linogaliana <linogaliana@yahoo.fr>
2021-09-03 16:03:37 +0000

    corrige erreur

--- build/setup.sh
+++ build/setup.sh
@@ -2,6 +2,6 @@
 
 REPO_URL=https://github.com/linogaliana/python-datascientist
 
-# Clone the repository in /home/rstudio/tutoriel
-mkdir /home/python-datascientist
-git clone $REPO_URL /home/python-datascientist
+# Clone the repository
+mkdir ~/python-datascientist
+git clone $REPO_URL ~/python-datascientist
